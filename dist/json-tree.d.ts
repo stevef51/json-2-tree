59cf3b3dfa27af314a260b6b9a350cefac640d83
Steve Fillingham <steve.fillingham@virtualmgr.com.au>
2020-04-01 23:14:43 +1100

    Generating declarations again

--- dist/json-tree.d.ts
+++ dist/json-tree.d.ts
@@ -2,40 +2,45 @@ declare type Flatten = (o: any, context?: any) => any;
 declare type Fatten = (o: any, context?: any) => any;
 interface TypeTranslator {
     ctr: Function;
-    nameOverride: string;
+    nameOverride?: string;
     flatten?: Flatten;
     fatten?: Fatten;
 }
+export declare const globalTypes: TypeTranslator[];
 export declare class JsonTree {
+    externs: any[];
+    types: TypeTranslator[];
+    constructor();
+    stringify(tree: any, context?: any): string;
+    parse(json: string, context?: any): any;
+    registerType(config: TypeTranslator): TypeTranslator;
+    static registerType(config: TypeTranslator): TypeTranslator;
     static stringify(tree: any, context?: any, externs?: any[]): string;
     static parse(json: string, context?: any, externs?: any[]): any;
-    static registerType(config: {
-        ctr: Function;
-        nameOverride?: string;
-        flatten?: Flatten;
-        fatten?: Fatten;
-    }): TypeTranslator;
 }
 export declare class Json2Tree {
     flattened: any[];
+    types: TypeTranslator[];
     context?: any;
-    externs?: any[] | undefined;
+    externs?: any[];
     fatObjects: any[];
     fattenedObjects: any;
-    constructor(flattened: any[], context?: any, externs?: any[] | undefined);
+    constructor(flattened: any[], types: TypeTranslator[], context?: any, externs?: any[]);
     fattenObject(flatObj: any): any;
     fattenArray(flatArray: []): any;
     fatten(flatRef: any): any;
     storeRef(fatObj: any, flatObj: any): any;
 }
 export declare class Tree2Json {
+    types: TypeTranslator[];
     context?: any;
-    externs?: any[] | undefined;
+    externs?: any[];
     flatObjects: any[];
     fatObjects: any[];
-    constructor(context?: any, externs?: any[] | undefined);
+    constructor(types: TypeTranslator[], context?: any, externs?: any[]);
     flattenObject(fatObj: any): any;
     flattenArray(fatArray: []): any;
+    flattenBasic(fatObj: any): any;
     flatten(fatObj: any): any;
     storeRef(flatObj: any, fatObj: any): number;
 }
